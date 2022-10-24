afd9b1820c98f7ffc260e7564bb97a637a0b8824
AJustinD <alexjustin0209@gmail.com>
2022-10-24 21:01:46 +0700

    50% complete

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public void LoadFirstLevel()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(1);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}

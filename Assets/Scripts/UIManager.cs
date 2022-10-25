00bc7afd64994b6105b6cd7ec5eb79a1d20643ca
AJustinD <alexjustin0209@gmail.com>
2022-10-25 14:34:57 +0700

    60 % Complete

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,12 +6,29 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-
+    private Button button;
     public void LoadFirstLevel()
     {
         DontDestroyOnLoad(gameObject);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1); 
     }
+
+    public void LoadMainLevel()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            button = GameObject.FindWithTag("QuitButton").GetComponent<Button>();
+            button.onClick.AddListener(LoadMainLevel);
+        }
+    }
+
+
 
     void Start()
     {
